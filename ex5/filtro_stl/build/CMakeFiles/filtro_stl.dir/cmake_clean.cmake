58a232704051c89e9f1deb3e192b757ca44a15a7
alxperezECI <alexander.perez@escuelaing.edu.co>
2020-09-25 10:12:12 -0500

    Cambiando archivos corruptos.

---
+++ ex5/filtro_stl/build/CMakeFiles/filtro_stl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/filtro_stl.dir/main.cpp.o"
+  "filtro_stl.pdb"
+  "filtro_stl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/filtro_stl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
